c000ddcb08f6e42b947fd7b617cdb193187cace5
Plong-Wasin <72617740+Plong-Wasin@users.noreply.github.com>
2020-10-17 20:38:50 +0700

    Update nekopostLoadList.user.js

--- nekopostLoadList.user.js
+++ nekopostLoadList.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Nekopost load list
 // @namespace    https://www.nekopost.net/
-// @version      1.4
+// @version      1.4.1
 // @description  Nekopost load list
 // @author       Wasin Phungwigrai
 // @include      https://www.nekopost.net/manga/
+// @include      https://www.nekopost.net//manga/
 // @include      https://www.nekopost.net/doujin/
 // @include      https://www.nekopost.net/novel
 // @updateURL    https://github.com/Plong-Wasin/Nekopost/raw/main/nekopostLoadList.user.js
