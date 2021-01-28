b78b69ae486a93c4306eaf0fa387d713d586046e
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-01-28 13:22:06 +0700

    Change to new function

--- nekopostLoadPage.user.js
+++ nekopostLoadPage.user.js
@@ -16,21 +16,50 @@ var imageAddr;
 var img;
 var count = 0;
 
-var btn = document.createElement("spam"); // Create a <button> element
-btn.innerHTML =
-    '<button class="btn btn-success btn-sm" onclick="window.location.href=\'https://www.nekopost.net/manga\'">Manga</button>';
-document
-    .getElementById("bottom-menu")
-    .insertBefore(btn, document.getElementById("bottom-menu").childNodes[0]);
-var getClass = document.getElementsByClassName("btn btn-success btn-sm");
-var countClass = getClass.length - 1;
-getClass[countClass].innerText = "X";
-getClass[countClass].style.backgroundColor = "red";
-getClass[countClass].style.borderColor = "red";
-getClass[countClass].onclick = function() {
-    window.close();
-};
+function createBtnBR() {
+    let btn = document.createElement("spam"); // Create a <button> element
+    btn.innerHTML =
+        '<button class="btn btn-success btn-sm" onclick="window.location.href=\'https://www.nekopost.net/manga\'">Manga</button>';
+    document
+        .getElementById("bottom-menu")
+        .insertBefore(btn, document.getElementById("bottom-menu").childNodes[0]);
+    var getClass = document.getElementsByClassName("btn btn-success btn-sm");
+    var countClass = getClass.length - 1;
+    getClass[countClass].innerText = "X";
+    getClass[countClass].style.backgroundColor = "red";
+    getClass[countClass].style.borderColor = "red";
+    getClass[countClass].onclick = function() {
+        window.close();
+    };
+}
 
+function createBtnB() {
+    if (document.getElementById("btnChangeChapterNextBottom").disabled) {
+        var btn2 = document.createElement("spam"); // Create a <button> element
+        btn2.innerHTML =
+            '<div id="manga" style="padding-bottom: 5px" class="mt-1 text-ellipsis"><span id="projectNameBottom"><a href="https://www.nekopost.net//manga/" class="btn btn-success">Manga</a></span></div>';
+        document
+            .getElementsByClassName("text-center mb-5")[0]
+            .insertBefore(
+                btn2,
+                document.getElementsByClassName("text-center mb-5")[0].childNodes[0]
+            );
+        var btn3 = document.createElement("spam"); // Create a <button> element
+        btn3.innerHTML =
+            ' <button id="btnClose" class="btn btn-success" style="display: hide;" onclick="closeNekopost()">Close</button>';
+        document
+            .getElementsByClassName("text-center mb-5")[0]
+            .insertBefore(
+                btn3,
+                document.getElementsByClassName("text-center mb-5")[0].childNodes[0]
+            );
+        document.getElementById("btnClose").style.backgroundColor = "red";
+        document.getElementById("btnClose").style.borderColor = "red";
+        document.getElementById("btnClose").onclick = function() {
+            window.close();
+        };
+    }
+}
 // setTimeout(function() {
 //     if (document.getElementById("btnChangeChapterNextBottom").disabled) {
 //         var btn2 = document.createElement("spam"); // Create a <button> element
@@ -60,32 +89,9 @@ getClass[countClass].onclick = function() {
 // }, 3000);
 
 $(document).ready(function() {
-    if (document.getElementById("btnChangeChapterNextBottom").disabled) {
-        var btn2 = document.createElement("spam"); // Create a <button> element
-        btn2.innerHTML =
-            '<div id="manga" style="padding-bottom: 5px" class="mt-1 text-ellipsis"><span id="projectNameBottom"><a href="https://www.nekopost.net//manga/" class="btn btn-success">Manga</a></span></div>';
-        document
-            .getElementsByClassName("text-center mb-5")[0]
-            .insertBefore(
-                btn2,
-                document.getElementsByClassName("text-center mb-5")[0].childNodes[0]
-            );
-        var btn3 = document.createElement("spam"); // Create a <button> element
-        btn3.innerHTML =
-            ' <button id="btnClose" class="btn btn-success" style="display: hide;" onclick="closeNekopost()">Close</button>';
-        document
-            .getElementsByClassName("text-center mb-5")[0]
-            .insertBefore(
-                btn3,
-                document.getElementsByClassName("text-center mb-5")[0].childNodes[0]
-            );
-        document.getElementById("btnClose").style.backgroundColor = "red";
-        document.getElementById("btnClose").style.borderColor = "red";
-        document.getElementById("btnClose").onclick = function() {
-            window.close();
-        };
-    }
     newGeneratePage();
+    createBtnB();
+    createBtnBR();
 });
 
 function newGeneratePage() {
@@ -95,7 +101,6 @@ function newGeneratePage() {
         lastPage[lastPage.length - 1].onload = function() {
             newGeneratePage();
         };
-        console.log("last");
     } catch (error) {
         setTimeout(() => {
             newGeneratePage();
